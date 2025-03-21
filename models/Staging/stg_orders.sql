bf5c64c294c26a7dc05959c6948bdfc0e7022d48
ravi siva <ravnisha@gmail.com>
2025-03-21 19:17:37 +0000

    developed report_profit_by_customer and report_profit_by_product

---
+++ models/Staging/stg_orders.sql
@@ -0,0 +1,24 @@
+select 
+--from raw order
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordercostprice,
+o.ordersellingprice,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+--from raw customer
+c.customerid,
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.productid,
+p.productname,
+p.category,
+p.subcategory
+from {{ ref('raw_order') }} as o
+join {{ ref('raw_customer') }} as c
+on o.customerid= c.customerid
+join {{ ref('raw_product') }} as p
+on o.productid = p.productid

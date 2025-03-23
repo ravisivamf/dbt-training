12920b54ecfd144accee79ed32930446a284086a
ravi siva <ravnisha@gmail.com>
2025-03-23 01:19:40 +0000

    added seeds

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -18,10 +18,14 @@ c.country,
 p.productid,
 p.productname,
 p.category,
-p.subcategory
+p.subcategory,
+--from deliver team
+d.delivery_team
 from {{ ref('raw_order') }} as o
 join {{ ref('raw_customer') }} as c
 on o.customerid= c.customerid
 join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
 {{ limit_data_in_dev('Furniture')}}
